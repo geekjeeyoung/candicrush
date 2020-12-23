9b4d4721042449f47864eeb43892c523b64cae47
Jeeyoung Han <64334381+hancandice@users.noreply.github.com>
2020-12-23 15:45:03 +0900

    ADD reducers

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -3,6 +3,10 @@ import {combineReducers} from 'redux';
 import {auth} from '../../Core/onboarding/redux/auth';
 import {feed} from '../../Core/socialgraph/feed/redux';
 import RootNavigator from '../../navigators/RootNavigator';
+import {chat} from '../../Core/chat/redux';
+import {friends} from '../../Core/socialgraph/friendships/redux';
+import {userReports} from '../../Core/user-reporting/redux';
+import {notifications} from '../../Core/notifications/redux';
 
 const LOG_OUT = 'LOG_OUT';
 
@@ -13,6 +17,10 @@ const appReducer = combineReducers({
   nav: navReducer,
   auth: auth,
   feed: feed,
+  chat: chat,
+  friends: friends,
+  userReports: userReports,
+  notifications: notifications,
 });
 
 const rootReducer = (state, action) => {
