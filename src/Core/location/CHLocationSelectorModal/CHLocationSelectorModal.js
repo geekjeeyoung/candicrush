ffae3f9a8b77009b5305ba129a39df214447f9d5
Jeeyoung Han <64334381+hancandice@users.noreply.github.com>
2020-12-31 14:36:31 +0900

    Update CHLocationSelectorModal.js

---
+++ src/Core/location/CHLocationSelectorModal/CHLocationSelectorModal.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+function CHLocationSelectorModal() {
+  return (
+    <View>
+      <Text>CHLocationSelectorModal</Text>
+    </View>
+  );
+}
+
+export default CHLocationSelectorModal;
