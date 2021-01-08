761d11ee4005070a07b4e191ae91a744451f99ef
Jeeyoung Han <64334381+hancandice@users.noreply.github.com>
2021-01-08 22:54:29 +0900

    Update tracker.js

--- src/Core/socialgraph/friendships/firebase/tracker.js
+++ src/Core/socialgraph/friendships/firebase/tracker.js
@@ -1,6 +1,10 @@
-import {setUserData} from '../../../onboarding/redux/auth';
+import {setUserData, setUsers} from '../../../onboarding/redux/auth';
 import {FriendshipConstants} from '../constants';
-import {setFriendsListenerDidSubscribe} from '../redux';
+import {
+  setFriends,
+  setFriendships,
+  setFriendsListenerDidSubscribe,
+} from '../redux';
 import {firebaseUser} from './../../../firebase';
 import * as firebaseFriendship from './friendship';
 import {reportingManager} from '../../../user-reporting';
@@ -208,11 +212,91 @@ export default class FriendshipTracker {
       hydratedUsers.forEach((user) => {
         usersHash[user.id] = user;
       });
-      // (Bookmark) const hydratedFriends = hydratedUsers.filter();
+      const hydratedFriends = hydratedUsers.filter(
+        (user) =>
+          outboundFriendsIDs[user.id] == true &&
+          inboundFriendsIDs[user.id] == true,
+      );
+      const friendshipsFromFriends = hydratedFriends.map((user) => {
+        return {
+          user: user,
+          type: 'reciprocal',
+        };
+      });
+      const friendshipsFromInbound = inboundFriendships
+        .filter(
+          (friendship) =>
+            friendsIDsHash[friendship.user1] != true &&
+            usersHash[friendship.user1],
+        )
+        .map((friendship) => {
+          return {
+            user: usersHash[friendship.user1],
+            type: 'inbound',
+          };
+        });
+
+      const friendshipsFromOutbound = outboundFriendships
+        .filter(
+          (friendship) =>
+            friendsIDsHash[friendship.user2] != true &&
+            usersHash[friendship.user2],
+        )
+        .map((friendship) => {
+          return {
+            user: usersHash[friendship.user2],
+            type: 'outbound',
+          };
+        });
+
+      let finalFriendships = [
+        ...friendshipsFromInbound,
+        ...friendshipsFromFriends,
+        ...friendshipsFromOutbound,
+      ].filter((friendship) => !bannedUserIDs.includes(friendship.user.id));
+
+      let dedupedFriendships = [];
+      let tempHash = {};
+      finalFriendships.forEach((friendship) => {
+        if (tempHash[friendship.user.id] != true) {
+          dedupedFriendships.push(friendship);
+          tempHash[friendship.user.id] = true;
+        }
+      });
+      finalFriendships = dedupedFriendships;
+
+      this.reduxStore.dispatch(setFriendships(finalFriendships));
+
+      const finalFriends = hydratedFriends.filter(
+        (friend) => !bannedUserIDs.includes(friend.id),
+      );
+      this.reduxStore.dispatch(setFriends(finalFriends));
     }
   }
 
-  updateUsers = (users) => {};
+  updateUsers = (users) => {
+    // We remove all friends and friendships from banned users
+    const state = this.reduxStore.getState();
+    const bannedUserIDs = state.userReports.bannedUserIDs;
 
-  purgeBannedUsers() {}
+    if (bannedUserIDs) {
+      this.users = users.filter((user) => !bannedUserIDs.includes(user.id));
+    } else {
+      this.users = users;
+    }
+    this.reduxStore.dispatch(setUsers(this.users));
+
+    this.hydrateFriendships();
+  };
+
+  purgeBannedUsers() {
+    const state = this.reduxStore.getState();
+    const bannedUserIDs = this.bannedUserIDs;
+    if (bannedUserIDs) {
+      const users = state.auth.users.filter(
+        (user) => !bannedUserIDs.includes(user.id),
+      );
+      this.reduxStore.dispatch(setUsers(users));
+    }
+  }
 }
